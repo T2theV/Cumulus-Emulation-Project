2d435a85ba4e39b1091706e005b46112e38963df
T2theV <t2thev@outlook.com>
2025-03-07 19:39:06 +0000

    adding for prerelease

--- images/webtop-files/defaults/startwm.sh
+++ images/webtop-files/defaults/startwm.sh
@@ -8,12 +8,15 @@ if which nvidia-smi; then
 fi
 
 sudo chown root:abc /dev/uinput
+sudo chmod 660 /dev/uinput
 sunshine &
 
 # Disable compositing
 setterm blank 0
 setterm powerdown 0
 gsettings set org.mate.Marco.general compositing-manager false
+gsettings set org.mate.screensaver idle-activation-enabled false
+gsettings set org.mate.screensaver lock-enabled false
 
 # Launch DE
 /usr/bin/mate-session > /dev/null 2>&1
