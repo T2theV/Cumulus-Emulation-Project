6bdceb4b80b62df99f0a36c418b426894fd06bf3
T2theV <t2thev@outlook.com>
2026-02-17 21:48:01 +0000

    got xemu building.

--- nix/xemu.nix
+++ nix/xemu.nix
@@ -4,16 +4,37 @@ let
   pkgs = import nixpkgs { config = {}; overlays = []; };
 in
 rec {
-  xemuLocal = pkgs.callPackage ./xemu-pkg.nix { };
-  xemuatts = builtins.fromJSON ( builtins.readFile ./xemu.json);
-  xemu-new = xemuLocal.overrideDerivation (oldAttrs: {
+  #xemuatts = builtins.fromJSON ( builtins.readFile ./xemu.json);
+  xemu-new = pkgs.xemu.overrideAttrs (finalAttrs: previousAttrs: {
       version = "master";
       src = pkgs.fetchFromGitHub {
-        owner = xemuatts.owner;
-        repo = xemuatts.repo;
-        rev = xemuatts.rev;
-        hash = xemuatts.hash;
-        fetchSubmodules = true;
+        owner = "xemu-project";
+        repo = "xemu";
+        rev = "3b13aed14ce6350c0cbd7696799b041037c91ece";
+        hash = "sha256-kTqg2Oo4bWyCkmcxAgE/bqCYAczPflKjwzrqveyG5H0=";
+
+        nativeBuildInputs = [
+          pkgs.git
+          pkgs.meson
+          pkgs.cacert
+        ];
+        # also fetch required git submodules
+        postFetch = ''
+          cd "$out"
+
+          meson subprojects download \
+            SPIRV-Reflect VulkanMemoryAllocator berkeley-softfloat-3 berkeley-testfloat-3 genconfig glslang imgui \
+            implot json keycodemapdb nv2a_vsh_cpu tomlplusplus volk xxhash sdl3 || true
+          find subprojects -type d -name .git -prune -execdir rm -r {} +
+        '';
       };
+      buildInputs = previousAttrs.buildInputs ++ [
+        pkgs.cacert 
+        pkgs.systemd 
+        pkgs.pipewire 
+        pkgs.libxkbcommon 
+        pkgs.wayland-scanner
+        ];
+      nativeBuildInputs = previousAttrs.nativeBuildInputs ++ [pkgs.wayland-scanner];
   });
 }
