5a03bc0d8c8ce9a2c702224e74d0d288880f1a6c
T2theV <t2thev@outlook.com>
2026-02-17 18:52:13 +0000

    got latest RPCS3 building

--- nix/rpcs3.nix
+++ nix/rpcs3.nix
@@ -1,20 +1,28 @@
 let
-  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-25.11";
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-unstable";
   pkgs = import nixpkgs { config = {}; overlays = []; };
 in
 rec{
-
-  rpcs3atts = builtins.fromJSON ( builtins.readFile ./rpcs3.json);
+  
+  #rpcs3atts = builtins.fromJSON ( builtins.readFile ./rpcs3.json);
   rpcs3-new = pkgs.rpcs3.overrideAttrs (finalAttrs: previousAttrs: {
       version = "master";
       src = pkgs.fetchFromGitHub {
-        owner = rpcs3atts.owner;
-        repo = rpcs3atts.repo;
-        rev = rpcs3atts.rev;
-        hash = rpcs3atts.hash;
-        fetchSubmodules = true;
+        owner = "rpcs3";
+        repo = "rpcs3";
+        rev = "b30a44c1367635af0675f61504e1ca45263655c6";
+        hash = "sha256-q6n5luMyWUYTLt8LkHEygY6vxYxXsYSAbu4YoZTnzzU=";
+
+        postCheckout = ''
+          cd $out/3rdparty
+          git submodule update --init \
+            fusion/fusion asmjit/asmjit yaml-cpp/yaml-cpp SoundTouch/soundtouch stblib/stb \
+            feralinteractive/feralinteractive protobuf
+        '';
+
       };
-    patches = [ ];
+      buildInputs = previousAttrs.buildInputs ++ [pkgs.protobuf];
+      nativeBuildInputs = previousAttrs.nativeBuildInputs ++ [pkgs.protobuf];
   });
 
 }
