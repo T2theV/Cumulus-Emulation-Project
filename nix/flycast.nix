4f517311f362905be64f5878a017b6a31fe37c16
T2theV <t2thev@outlook.com>
2026-02-18 16:04:16 +0000

    build flycast

--- nix/flycast.nix
+++ nix/flycast.nix
@@ -3,17 +3,17 @@ let
   nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-unstable";
   pkgs = import nixpkgs { config = {}; overlays = []; };
 in
-rec {
-  flycastLocal = pkgs.callPackage ./flycast-pkg.nix { };
-  flycastatts = builtins.fromJSON ( builtins.readFile ./flycast.json);
-  flycast-new = flycastLocal.overrideDerivation (oldAttrs: {
+{
+  flycast-new = pkgs.flycast.overrideAttrs (finalAttrs: previousAttrs: {
       version = "master";
       src = pkgs.fetchFromGitHub {
-        owner = flycastatts.owner;
-        repo = flycastatts.repo;
-        rev = flycastatts.rev;
-        hash = flycastatts.hash;
+        owner = "flyinghead";
+        repo = "flycast";
+        rev = "ba5b3c71ecc966e52f698f41443e7cc9b81bf824";
+        hash = "sha256-tbq+NgbZDKMg0K0cWF1+7h80QTaAaO5BD9nf94z5fc0=";
         fetchSubmodules = true;
       };
+      buildInputs = previousAttrs.buildInputs ++ [pkgs.systemd];
+      cmakeFlags = previousAttrs.cmakeFlags ++ [ "-DUSE_OPENGL=OFF" ]
   });
 }
