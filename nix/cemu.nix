1f8b68b66094a974daa8076349d3ffaf7581dd5e
T2theV <t2thev@outlook.com>
2026-02-18 17:16:40 +0000

    finishexd xenia and menu needs patches

--- nix/cemu.nix
+++ nix/cemu.nix
@@ -3,17 +3,22 @@ let
   nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-unstable";
   pkgs = import nixpkgs { config = {}; overlays = []; };
 in
-rec {
-  cemuLocal = pkgs.callPackage ./cemu-pkg.nix { };
-  cemuatts = builtins.fromJSON ( builtins.readFile ./cemu.json);
-  cemu-new = cemuLocal.overrideDerivation (oldAttrs: {
-      version = "master";
-      src = pkgs.fetchFromGitHub {
-        owner = cemuatts.owner;
-        repo = cemuatts.repo;
-        rev = cemuatts.rev;
-        hash = cemuatts.hash;
-        fetchSubmodules = true;
+{
+  #cemuatts = builtins.fromJSON ( builtins.readFile ./cemu.json);
+  cemu-new = pkgs.cemu.overrideAttrs (finalAttrs: previousAttrs: {
+      version = "99.99";
+       src = pkgs.fetchFromGitHub {
+        owner = "cemu-project";
+        repo = "Cemu";
+        rev = "3b5c6f5246e1e94bda623f618f71a37de22578b3";
+        hash = "sha256-b/mqd/OwJiyoUG59gZ1+LFVXQP+hwkHNx04Spz+2jn4=";
       };
+      patches = [];
+      buildInputs = previousAttrs.buildInputs ++ [
+        pkgs.wxwidgets_3_3
+      ];
+      nativeBuildInputs = [
+        pkgs.wxwidgets_3_3
+      ] ++ previousAttrs.nativeBuildInputs;
   });
 }
--- nix/xenia-canary.nix
+++ nix/xenia-canary.nix
@@ -2,18 +2,18 @@ let
   nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-unstable";
   pkgs = import nixpkgs { config = {}; overlays = []; };
 in
-rec{
+{
 
-  xenia-canaryLocal = pkgs.callPackage ./xenia-canary-pkg.nix { };
-  xeniaatts = builtins.fromJSON ( builtins.readFile ./xenia.json);
-  xenia-new = xenia-canaryLocal.overrideDerivation (oldAttrs: {
+  
+  #xeniaatts = builtins.fromJSON ( builtins.readFile ./xenia.json);
+  xenia-new = pkgs.xenia-canary.overrideAttrs (finalAttrs: previousAttrs: {
       version = "master";
       src = pkgs.fetchFromGitHub {
-        owner = xeniaatts.owner;
-        repo = xeniaatts.repo;
-        rev = xeniaatts.rev;
-        hash = xeniaatts.hash;
+        owner = "xenia-canary";
+        repo = "xenia-canary";
         fetchSubmodules = true;
+        rev = "07501cfcb9f592c3f99eeb7a8d8f10294755a980";
+        hash = "sha256-dacGpGAK4At0KXdANY6ibEAjxWcY4bUbyFraXO+kK/0=";
       };
   });
 
