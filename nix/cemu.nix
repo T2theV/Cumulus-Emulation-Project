7e1665e156f1cfae056a90224f614e3ebb05366c
T2theV <t2thev@outlook.com>
2026-02-18 18:25:01 +0000

    some stupid cmake stuff. probably whole package needs to be rewritten to be clang compiled.

--- nix/cemu.nix
+++ nix/cemu.nix
@@ -13,7 +13,9 @@ in
         rev = "3b5c6f5246e1e94bda623f618f71a37de22578b3";
         hash = "sha256-b/mqd/OwJiyoUG59gZ1+LFVXQP+hwkHNx04Spz+2jn4=";
       };
-      patches = [];
+      patches = [
+        "/work/imgui.patch"
+      ];
       buildInputs = previousAttrs.buildInputs ++ [
         pkgs.wxwidgets_3_3
       ];
