dc0323c6ff3854bbccf10ae2c382a1614b3db719
T2theV <t2thev@outlook.com>
2026-03-06 22:43:43 +0000

    xemu build attempt

--- nix/xemu.nix
+++ nix/xemu.nix
@@ -7,34 +7,15 @@ rec {
   # xemuatts = builtins.fromJSON ( builtins.readFile ./xemu.json);
   xemu-new = pkgs.xemu.overrideAttrs (finalAttrs: previousAttrs: {
       version = "99.99.99";
-      src = pkgs.fetchFromGitHub {
-        owner = "xemu-project";
-        repo = "xemu";
+      src = previousAttrs.src.overrideAttrs(finalAttrs: previousAttrs: {
         rev = "5aeacacfeb7a28ad7d5a9ecab9978115804148dc";
-        hash = "sha256-2ry9+Hniyz1mDr1wdtoi0bg5ks46GwF9r9s3qzsSHQY=";
-
-        nativeBuildInputs = [
-          pkgs.git
-          pkgs.meson
-          pkgs.cacert
-        ];
-        # also fetch required git submodules
-        postFetch = ''
-          cd "$out"
-
-          meson subprojects download \
-            SPIRV-Reflect VulkanMemoryAllocator berkeley-softfloat-3 berkeley-testfloat-3 genconfig glslang imgui \
-            implot json keycodemapdb nv2a_vsh_cpu tomlplusplus volk xxhash sdl3 || true
-          find subprojects -type d -name .git -prune -execdir rm -r {} +
-        '';
-      };
-      buildInputs = previousAttrs.buildInputs ++ [
-        pkgs.cacert 
-        pkgs.systemd 
-        pkgs.pipewire 
-        pkgs.libxkbcommon 
-        pkgs.wayland-scanner
-        ];
-      nativeBuildInputs = previousAttrs.nativeBuildInputs ++ [pkgs.wayland-scanner];
+        hash = "sha256-oC44EMr71J6abDeGXtFAK68Ti/zi+CerxjFRALOoCwE=";
+      });
+      configureFlags = previousAttrs.configureFlags ++ [
+          "--disable-opengl"
+          "--enable-gtk"
+      ];
+      buildInputs = previousAttrs.buildInputs ++ [pkgs.pixman];
+      nativeBuildInputs  = previousAttrs.nativeBuildInputs  ++ [pkgs.pixman];
   });
 }
