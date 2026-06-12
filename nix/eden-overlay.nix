cbb34176ccfa0261a5732bd7b857fee5552fb1d7
Tim VanHove <t2thev@outlook.com>
2026-06-12 21:58:36 +0000

    fixing eden

--- nix/eden-overlay.nix
+++ nix/eden-overlay.nix
@@ -3,7 +3,7 @@ final: prev:
   eden = prev.eden.overrideAttrs (old: {
       src = prev.eden.src.overrideAttrs(old: {
             tag=0; 
-            rev="772e38cb8d9d1076305e7f02a4434c2ab54ae249";
+            rev="ad9af2502712ad41ae29ed078b332ff1a6875ae8";
 #( builtins.fromJSON( builtins.readFile ./eden-out.json)).rev;
         });
   });
