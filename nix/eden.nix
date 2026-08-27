d644250624cb7458a0c65336b98523f325387785
T2theV <t2thev@outlook.com>
2026-08-27 14:28:52 +0000

    got debug and build in docker

--- nix/eden.nix
+++ nix/eden.nix
@@ -4,19 +4,18 @@ let
   pkgs = import nixpkgs { config = {}; overlays = [ (import ./eden-overlay.nix) ]; };
 in
 rec{
-  #edenatts = builtins.fromJSON ( builtins.readFile ./eden-full-out.json);
+  edenatts = builtins.fromJSON ( builtins.readFile ./eden-full-out.json);
   eden-src = pkgs.eden.src.overrideAttrs(finalAttrs: previousAttrs: { 
-   hash = "sha256-YljgWKS12iOXFo8CI43dYsJtLsxyy9MjxQrNHK1Dxh0=";
-#edenatts.hash;
+   hash = edenatts.hash;
   });
   eden-new = pkgs.eden.overrideAttrs (finalAttrs: previousAttrs: {
       version = "master";
       src = eden-src;
-      patches = [];
       nativeBuildInputs = [ pkgs.kdePackages.qtcharts
 			    pkgs.sdl3
 			 ] ++ previousAttrs.nativeBuildInputs;
       buildInputs = [ pkgs.sdl3 ] ++ previousAttrs.buildInputs;
       postInstall = "";
+      postConfigure = "";
   });
 }
